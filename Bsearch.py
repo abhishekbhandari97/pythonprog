eb717da5148b62c819a1cf3495a76a9c82d4afaa
Abhishek Bhandari <62231874+abhishekbhandari97@users.noreply.github.com>
2020-04-06 15:24:43 +0530

    code added

--- Bsearch.py
+++ Bsearch.py
@@ -1,1 +1,29 @@
-print("hello")
+pos = -1
+
+
+def search(nums, n):
+    f = 0
+    u = len(nums) - 1
+
+    while f <= u:
+        mid = (f + u) // 2
+
+        if nums[mid] == n:
+            globals()['pos'] = mid
+            return True
+        else:
+            if nums[mid] < n:
+                f = mid + 1
+            else:
+                u = mid - 1
+
+
+nums = [2, 3, 4, 5, 6, 11, 13, 44, 55, 77, 88, 99]
+n = int(input("enter the number you want to search"))
+
+if search(nums, n):
+
+    print("value is found at", pos + 1)
+
+else:
+    print("value is not found")
